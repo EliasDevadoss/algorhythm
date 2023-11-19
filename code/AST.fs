4477f69512e3697f496404ae3efe5362bc872a0d
Himal Pandey <hrp3@williams.edu>
2023-11-19 13:53:45 -0500

    added AST

--- code/AST.fs
+++ code/AST.fs
@@ -1,1 +1,29 @@
 module AST
+
+type Num = int
+
+type Tempo = Num
+
+type NoteType = Num
+
+type Meter = Num * NoteType
+
+type Accidental = Sharp | Flat | Natural
+
+type Pitch = char * Accidental * Num
+
+type Duration = Num
+
+type Note = Pitch * Duration
+
+type Sound = Kick | Snare | HiHat | Crash | Ride | China | Splash
+
+type Percuss = Sound * Num list
+
+type Chord = Pitch list * Duration
+
+type Melody = Note list
+
+type Beat = Percuss list
+
+type Song = Tempo * Meter * Melody * Beat * Chord list
