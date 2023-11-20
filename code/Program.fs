5c792c3cb712671bf8f6823bbf5674628734cf58
Elias Devadoss <eliasdevad@gmail.com>
2023-11-19 20:50:07 -0500

    minor changes

--- code/Program.fs
+++ code/Program.fs
@@ -1,2 +1,16 @@
 open Parser
 open Evaluator
+
+[<EntryPoint>]
+let main (argv: string array) =
+   if argv.Length = 1 then
+       match parse argv.[0] with
+       | Some _____ ->
+           
+           0
+       | None ->
+           usage()
+           1
+   else
+       usage()
+       1
